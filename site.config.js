8e36b47060966f6c63dce2b5b61cbaf6805bfe40
Fynn Laue <lachhaft@gmail.com>
2021-03-27 15:01:17 +0100

    update domain

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: 'FYNN - code & VFX',
-  domain: 'notion-hp.vercel.app',
+  domain: 'fynn-notion-hp.vercel.app',
   author: 'Fynn Laue',
 
   // open graph metadata (optional)
